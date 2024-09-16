bdded79763b9769d5beb2cbd97520b298e75b168
Ishaan005 <ishaan.arakkal@gmail.com>
2024-09-16 19:15:56 +0100

    Dynamic paging and Title change

--- app/api/emails/stats/route.ts
+++ app/api/emails/stats/route.ts
@@ -2,6 +2,8 @@ import { prisma } from "@/lib/prisma";
 import { NextRequest, NextResponse } from "next/server";
 import { getAuth } from "@clerk/nextjs/server"
 
+export const dynamic = 'force-dynamic';
+
 export async function GET(request: NextRequest) {
   try {
     const { userId } = getAuth(request);
